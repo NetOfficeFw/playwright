98284fdca6dcb66009d2c4a32a7f78103ad58d54
Jozef Izso <izso@cisco.com>
2024-12-18 13:25:33 +0100

    Allow testing the Sidebar

--- packages/playwright-mso-core/src/powerpoint.ts
+++ packages/playwright-mso-core/src/powerpoint.ts
@@ -1,4 +1,5 @@
-import { test as base } from '@playwright/test';
+import { playwright } from '@playwright/test';
+import { test as base, BrowserType, ChromiumBrowser } from '@playwright/test';
 import { ulid } from 'ulid';
 
 import { PowerPointApp, Presentation } from './../types/index'
@@ -9,6 +10,8 @@ type PowerPointTestFixture = {
   powerpoint: PowerPointApp;
 
   presentation: Presentation;
+
+  browser: BrowserType<ChromiumBrowser>;
 }
 
 type PowerPointWorkerFixture = {
@@ -20,8 +23,8 @@ export const test = base.extend<PowerPointTestFixture, PowerPointWorkerFixture>(
     const port = 53080;
     var provider = new PowerPointAppProviderImpl(sessionId, port);
 
-    const powerpoint = await provider.launch();
-    // const powerpoint = await provider.connectOverGrpc(`http://127.0.0.1:${port}`);
+    // const powerpoint = await provider.launch();
+    const powerpoint = await provider.connectOverGrpc(`http://127.0.0.1:${port}`);
 
     await use(powerpoint);
     await powerpoint.close();
@@ -33,5 +36,17 @@ export const test = base.extend<PowerPointTestFixture, PowerPointWorkerFixture>(
     }
 
     await use(presentation);
+  },
+  browser: async ({ playwright }, use, testInfo) => {
+    const browser = await playwright.chromium.connectOverCDP(`http://127.0.0.1:1236`);
+    await use(browser);
+  },
+  context: async ({ browser }, use) => {
+    const context = browser.contexts()[0];
+    await use(context);
+  },
+  page: async ({ context }, use) => {
+    const page = context.pages()[0];
+    await use(page);
   },
 });
--- tests/powerpoint/01_application.spec.ts
+++ tests/powerpoint/01_application.spec.ts
@@ -5,3 +5,14 @@ test('test Microsoft PowerPoint app', async ({ powerpoint }) => {
 
   await delay(2000);
 });
+
+test('browser test', async ({ page }) => {
+  const title = await page.title();
+  await expect(title).toEqual('Slido');
+
+//   await page.getByTestId('loginButton').click();
+
+  await page.getByTestId('card-clickable-overlay').nth(4).click();
+  await page.getByTestId('pollQuestionTitle').fill('Open text poll');
+  await page.getByTestId('submitPollFormButton').click();
+});
